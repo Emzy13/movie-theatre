37422d037b48b79b221c485505b6813d12549ed4
Ema Teodora <110203236+Emzy13@users.noreply.github.com>
2023-11-30 18:47:58 +0200

    backend

---
+++ movietheater/src/main/java/dev/emmzzit/movietheater/MovietheaterApplication.java
@@ -0,0 +1,15 @@
+package dev.emmzzit.movietheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MovietheaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietheaterApplication.class, args);
+	}
+
+
+}
